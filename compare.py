79af1a57796572a06b7e46bd7c42e4b1017497b0
Bunga Razvan <razvanbunga@gmail.com>
2022-10-30 12:52:51 +0000

    clarify variabiles.

--- compare.py
+++ compare.py
@@ -8,21 +8,22 @@ if __name__ == '__main__':
     file_to_read = [x for x in os.listdir(download_dir_path) if x.startswith('foo')][0]
     main_file_path = f'{main_dir_path}/playlists.json'
 
+    new_songs = []
+    previous_songs = []
+
 
     with open(main_file_path, 'r') as file:
-        f2_songs = []
-        f1_songs = []
-        file_data = json.load(file)
+        previous_data = json.load(file)
 
         with open(f'{download_dir_path}/{file_to_read}', 'r') as f2:
-            file_read_data = json.load(f2)
+            new_read_data = json.load(f2)
 
-            for key, value in file_read_data.items():
-                f2_songs = value
+            for key, value in new_read_data.items():
+                new_songs = value
             
-            f1_songs = file_data[list(file_read_data.keys())[0]]
+            previous_songs = previous_data[list(new_read_data.keys())[0]]
             os.remove(f'{download_dir_path}/{file_to_read}')
-        
-        for song in f1_songs:
-            if song not in f2_songs:
+
+        for song in previous_songs:
+            if song not in new_songs:
                 print(song)
