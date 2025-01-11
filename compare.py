68b21245ab449fc366f987913d515d2b7d801e69
Bunga Razvan <razvanbunga@gmail.com>
2025-01-11 00:56:59 +0000

    multiple file support.

--- compare.py
+++ compare.py
@@ -5,7 +5,7 @@ import json
 if __name__ == '__main__':
     main_dir_path = '/Users/razvanbunga/Desktop/documents/playlists'
     download_dir_path = '/Users/razvanbunga/Downloads/'
-    file_to_read = [x for x in os.listdir(download_dir_path) if x.startswith('foo')][0]
+    files_to_read = [x for x in os.listdir(download_dir_path) if x.endswith('_songs.json')]
     main_file_path = f'{main_dir_path}/playlists.json'
 
     new_songs = []
@@ -15,13 +15,15 @@ if __name__ == '__main__':
     with open(main_file_path, 'r') as file:
         previous_data = json.load(file)
 
-        with open(f'{download_dir_path}/{file_to_read}', 'r') as f2:
-            new_read_data = json.load(f2)
+        for file in files_to_read:
+            with open(f'{download_dir_path}/{file}', 'r') as f2:
+                new_read_data = json.load(f2)
 
-            for key, value in new_read_data.items():
-                new_songs = value
-            
-            previous_songs = previous_data[list(new_read_data.keys())[0]]
-            os.remove(f'{download_dir_path}/{file_to_read}')
+                for key, value in new_read_data.items():
+                    new_songs = value
+                
+                previous_songs = previous_data[list(new_read_data.keys())[0]]
+                os.remove(f'{download_dir_path}/{file}')
 
-        print([s for s in previous_songs if not any(x in s for x in new_songs)])
+            print(file)
+            print(json.dumps([s for s in previous_songs if not any(x in s for x in new_songs)], indent=2))
--- playlists.py
+++ playlists.py
@@ -6,22 +6,23 @@ if __name__ == '__main__':
     main_dir_path = '/Users/razvanbunga/Desktop/documents/playlists'
     download_dir_path = '/Users/razvanbunga/Downloads/'
     main_file_path = f'{main_dir_path}/playlists.json'
-    files_to_read = [x for x in os.listdir(download_dir_path) if x.startswith('foo')]
+    files_to_read = [x for x in os.listdir(download_dir_path) if x.endswith('_songs.json')]
     exist_main_file = os.path.isfile(main_file_path)
     file_operation = 'w'
 
     if exist_main_file:
         file_operation = 'r+'
 
-    with open(main_file_path, file_operation) as file:
+    with open(main_file_path, file_operation, encoding='utf-8') as file:
         file_data = {}
 
         if exist_main_file:
+            file.seek(0)
             file_data = json.load(file)
 
         for file_to_read in reversed(files_to_read):
             print(file_to_read)
-            with open(f'{download_dir_path}/{file_to_read}', 'r') as f2:
+            with open(f'{download_dir_path}/{file_to_read}', 'r', encoding='utf-8') as f2:
                 file_read_data = json.load(f2)
 
                 for key, value in file_read_data.items():
@@ -30,6 +31,6 @@ if __name__ == '__main__':
                 os.remove(f'{download_dir_path}/{file_to_read}')
 
         file.seek(0)
-        json.dump(file_data, file, ensure_ascii=False, indent=4)
+        json.dump(file_data, file, ensure_ascii=False, indent=2)
 
     print(f'{len(files_to_read)} files combined')
